8d6d90124ea88fec26e3a8bbac1b4ae074cc67a5
Anh Tran <anhxuan.tran@gmail.com>
2013-08-26 13:01:50 -0700

    Renamed project

--- project/Build.scala
+++ project/Build.scala
@@ -4,14 +4,14 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "todo-webapp"
+  val appName         = "todo-play"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm,
-    "postgresql" % "postgresql" % "8.4-702.jdbc4"
+    anorm
+    //"postgresql" % "postgresql" % "8.4-702.jdbc4"
   )
 
 
